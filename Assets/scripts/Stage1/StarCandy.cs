3eb429729ed85c26a84f1dc78e84f80c6788d402
ChangHyeon Kim <hazasue@gmail.com>
2023-04-08 20:54:10 +0900

    :tada: Initialize StarCandy Script: Using in Stage1

---
+++ Assets/scripts/Stage1/StarCandy.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StarCandy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider player)
+    {
+        if(player.tag == "Player")
+        {
+            // TODO: move to main stage scene
+            SceneManager.LoadScene("Stage Entrance");
+            Destroy(this.gameObject);
+        }
+    }
+}
