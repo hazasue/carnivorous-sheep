83a094cc052278ade1c962bb5cb369ea55145986
ChangHyeon Kim <hazasue@gmail.com>
2023-05-08 14:15:23 +0900

    :tada: Initialize UIManager Script

---
+++ Assets/scripts/Controller/UIManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject clearScreen;
+    public GameObject failedScreen;
+    
+    private GameManager mGameManager;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Init()
+    {
+        mGameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    public void ActiveClearScreen()
+    {
+        if (clearScreen.activeSelf == true) return;
+        mGameManager.GetPlayer().GetComponent<Player>().SetGameOver(true);
+        clearScreen.SetActive(true);
+    }
+
+    public void UnActiveFailedScreen()
+    {
+        if (failedScreen.activeSelf == true) return;
+        mGameManager.GetPlayer().GetComponent<Player>().SetGameOver(true);
+        failedScreen.SetActive(true);
+    }
+
+    public void MoveToStageEntrance()
+    {
+        SceneManager.LoadScene("Stage Entrance");
+    }
+}
