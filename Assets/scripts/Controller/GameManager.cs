679deec5ce0496e02609e439141e0fd64f708d14
ChangHyeon Kim <hazasue@gmail.com>
2023-05-27 12:58:30 +0900

    :hammer: Update GameManager: Play Sound Resources when stage cleared/failed

--- Assets/scripts/Controller/GameManager.cs
+++ Assets/scripts/Controller/GameManager.cs
@@ -8,8 +8,11 @@ public class GameManager : MonoBehaviour
     private GameObject player;
 
     private UIManager mUIManager;
+    private SoundManager mSoundManager;
 
     private GameObject starCandy;
+
+    private bool isCheckedClear;
     
     // Start is called before the first frame update
     void Awake()
@@ -29,27 +32,36 @@ public class GameManager : MonoBehaviour
         
         player = GameObject.Find("Player");
         mUIManager = GameObject.Find("UIManager").GetComponent<UIManager>();
+        mSoundManager = GameObject.Find("SoundManager").GetComponent<SoundManager>();
 
         if (SceneManager.GetActiveScene().name == "Stage1")
         {
             starCandy = GameObject.Find("StarCandy");
         }
+
+        isCheckedClear = false;
     }
 
     private void CheckClear()
     {
+        if (isCheckedClear == true) return;
+        
         if (SceneManager.GetActiveScene().name == "Stage1")
         {
             if (starCandy == null)
             {
+                isCheckedClear = true;
                 mUIManager.ActiveClearScreen();
+                mSoundManager.SuccessStage();
             }
         } 
         else if (SceneManager.GetActiveScene().name == "Stage4")
         {
             if (player.GetComponent<PlayerInStage4>().GetCleared() == true)
             {
+                isCheckedClear = true;
                 mUIManager.ActiveClearScreen();
+                mSoundManager.SuccessStage();
             }
         }
     }
