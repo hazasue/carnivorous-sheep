6e4abb5f938357400099260b2d05f73166a9debc
ChangHyeon Kim <hazasue@gmail.com>
2023-05-27 12:59:06 +0900

    :hammer: Update SoundManager: Mapping AudioSource, sounds, Initialize

--- Assets/scripts/Controller/SoundManager.cs
+++ Assets/scripts/Controller/SoundManager.cs
@@ -1,30 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SoundManager : MonoBehaviour
 {
-    private Animator animator;
+    private static SoundManager instance;
+    public AudioSource BGM;
+    public AudioSource SFX;
     
-    private AudioSource audioSound;
     public AudioClip bgm;
+
+    public AudioClip success;
+    public AudioClip fail;
+    public AudioClip walk;
+    public AudioClip jump;
     
     // Start is called before the first frame update
     void Awake()
     {
-        animator = GetComponent<Animator>();
-        this.audioSound = this.gameObject.AddComponent<AudioSource>();
-        this.audioSound.volume = 0.33f;
-        this.audioSound.loop = true;
+        instance = this;
+        
+        BGM.volume = 0.33f;
+        SFX.volume = 0.33f;
+
+        BGM.loop = true;
+        SFX.loop = false;
+        SFX.playOnAwake = false;
+
+        SFX.priority = 0;
+        BGM.priority = 10;
         
         if (bgm == null) return;
-        audioSound.clip = bgm;
-        audioSound.Play();
+        BGM.clip = bgm;
+        BGM.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public static SoundManager GetInstance()
+    {
+        if (instance != null) return instance;
+        instance = FindObjectOfType<SoundManager>();
+
+        return instance;
+    }
+
+    public void SuccessStage()
+    {
+        SFX.clip = success;
+        SFX.Play();
+    }
+
+    public void FailStage()
+    {
+        SFX.clip = fail;
+        SFX.Play();
+    }
+
+    public void Walk()
+    {
+        SFX.loop = true;
+        SFX.clip = walk;
+        SFX.Play();
+    }
+
+    public void StopWalk()
+    {
+        SFX.loop = false;
+        SFX.Stop();
+    }
+
+    public void Jump()
+    {
+        SFX.clip = jump;
+        SFX.Play();
     }
 }
