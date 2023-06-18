bb99e3c5ee5289a7bc85d23405eef01837fe2961
ChangHyeon Kim <hazasue@gmail.com>
2023-06-19 04:47:18 +0900

    :hammer: Update Ingredient Script: Rotate object automatically

--- Assets/scripts/Stage4/Ingredients.cs
+++ Assets/scripts/Stage4/Ingredients.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class Ingredients : MonoBehaviour
 {
+    private float rotY;
+        
     // Start is called before the first frame update
     void Start()
     {
-        
+        rotY = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        rotY += Time.deltaTime * 20;
+        if (rotY >= 360f) rotY -= 360f;
+        this.transform.rotation = Quaternion.Euler(new Vector3(0f, rotY, 0f));
     }
 
     public void OnTriggerEnter(Collider player)
